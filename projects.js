c2021b8434be17dde372aa85a4c1462b6686d03e
wowdog15 <122061017+LilPumpkinXD@users.noreply.github.com>
2025-05-01 16:20:07 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -15,4 +15,11 @@ const projects = [
         link: "https://nestrix-applications.github.io/Auto-Blob-Arena/",
         lang: 'js',
     },
+    {
+        id: 2,
+        name: "Sapphire Game Engine",
+        description: "A lightweight and extensible 2D game engine designed for making simple games.",
+        link: "https://github.com/LilPumpkinXD/Sapphire-Game-Engine/blob/main/README.md",
+        lang: 'js',
+    },
 ];
