06117e40527b1a264046f52a6eb2e17d0463bf75
wowdog15 <122061017+LilPumpkinXD@users.noreply.github.com>
2025-04-30 21:00:05 -0400

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,16 @@
+// weird and edgy approach to store the data...
+
+const projects = [
+    {
+        id: 0,
+        name: "Project Tracker",
+        description: "An effective way to track scheduled deadlines, and make lists.",
+        link: ""
+    },
+    {
+        id: 1,
+        name: "Auto Blob Arena",
+        description: "A personal portfolio to showcase projects and skills.",
+        link: "https://github.com/Nestrix-Applications/Auto-Blob-Arena"
+    },
+];
---
+++ script.js
@@ -0,0 +1,13 @@
+function generateProjectHTML(project) {
+    return `
+        <div class="content-1">
+            <p>${project.name} <span class="java"></span></p>
+            <p class="description">${project.description}</p>
+            <button onclick="window.location.href='${project.link}'">View</button>
+        </div>
+    `;
+}
+const container = document.getElementById('projects');
+projects.forEach(project => {
+    container.innerHTML += generateProjectHTML(project);
+});
