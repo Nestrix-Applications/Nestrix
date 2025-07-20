96da8aeab7f92a3583607eb9207064e1badf48bf
Callen Flynn <not.skittle.or.beluga@gmail.com>
2025-07-20 01:36:01 -0400

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,65 +1,70 @@
-// weird and edgy approach to store the data...
-// ^ wut?
 
 const projects = [
-            {
+                        {
         id: 0,
+        name: "HellCrank",
+        description: "High octane FPS game (not out yet)",
+        link: "https://hellcrank.vercel.app/",
+        lang: 'exe',
+    },
+            {
+        id: 1,
         name: "Snake Gaem",
         description: "Funny little snake. A classic game. Made by Cal.",
         link: "https://github.com/callenflynn/snake-gaem/releases",
         lang: 'Csharp',
     },
     {
-        id: 1,
+        id: 2,
         name: "Hangman RS",
         description: "Hangman coded in Rust. Made by Cal.",
         link: "https://github.com/callenflynn/hangman-rs/releases",
         lang: 'rust',
     },
     {
-        id: 2,
+        id: 3,
         name: "Tic Tac Toe",
         description: "Tic Tac Toe against AI. Easy mode and Hard mode. Made by Cal.",
         link: "https://callenflynn.github.io/TicTacToe/",
         lang: 'website',
     },
     {
-        id: 3,
+        id: 4,
         name: "Project Tracker",
         description: "An effective way to track scheduled deadlines, and make lists. Made by Cal",
         link: "https://github.com/Nestrix-Applications/project-tracker/",
         lang: "java",
     },
     {
-        id: 4,
+        id: 5,
         name: "Auto Blob Arena",
         description: "Created By Wowdog15. A fun IDLE game. Made by Cal.",
         link: "https://nestrix-applications.github.io/Auto-Blob-Arena/",
         lang: 'website',
     },
     {
-        id: 5,
+        id: 6,
         name: "Sapphire Game Engine",
         description: "A lightweight and extensible 2D game engine designed for making simple games. Made by Wowdog15.",
         link: "https://github.com/LilPumpkinXD/Sapphire-Game-Engine/",
         lang: 'js',
     },
     {
-        id: 6,
+        id: 7,
         name: "Bit Bat Toe",
         description: "Tic Tac Toe against AI coded in Batch because why the hell not? Made by Cal.",
         link: "https://github.com/callenflynn/Bit-Bat-Toe/releases",
         lang: 'batch',
     },
     {
-        id: 7,
+        id: 8,
         name: "Hexa",
         description: "A text-based rpg where you can fight monsters and explore. Made by Cal.",
         link: "https://github.com/Nestrix-Applications/Hexa",
         lang: 'java',
     },
     {
-        id: 8,
+        id: 9,
         name: "Myspace (prank)",
         description: "A site that pretends to grab your ip! shows your location, satelite view of your location, and system specs. DOES NOT SAVE INFO",
         link: "https://myspace.not-skittle-or-beluga.workers.dev/",
